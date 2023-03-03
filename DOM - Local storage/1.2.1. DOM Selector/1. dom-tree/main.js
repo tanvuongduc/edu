10a2d08c165e1e0f4e4350b99d7da94fcf84f068
Nguyen Tuan Phong <tuanphong5801@gmail.com>
2023-03-03 20:06:20 +0700

    ahihi

--- DOM - Local storage/1.2.1. DOM Selector/1. dom-tree/main.js
+++ DOM - Local storage/1.2.1. DOM Selector/1. dom-tree/main.js
@@ -17,34 +17,9 @@
 // console.log(a, b);
 let a = 2;
 let b = 3;
-<<<<<<< HEAD
-let d = 4;
-let e = 5;
-let users = [ 
-    {
-        id: 1,
-        name: 'tan',
-        gender: true,
-    },
-    {
-        id: 2,
-        name: 'phuong',
-        gender: false,
-    },
-    {
-        id: 3,
-        name: 'thanh',
-        gender: true,
-    },
-    {
-        id: 4,
-        name: 'minh',
-        gender: true,
-    },
-]
-=======
 let cat = "love dog";
 let dog = "love cat";
+let dogAndCat = "we are lover";
 let users = [
   {
     id: 1,
@@ -67,7 +42,6 @@ let users = [
     gender: true,
   },
 ];
->>>>>>> 649251a1a3329fe278b54a2b7d9bf0a776cb5962
 let lifts = [
   {
     id: "1",
@@ -153,110 +127,105 @@ let actions = [
     time: new Date(2023, 1, 28).getTime(),
   },
 ];
-<<<<<<< HEAD
-
-let container = document.getElementById("container");
-=======
->>>>>>> 56e29f330a0bed609cb4e80ccb5d673ea1f228ec
 
 let container = document.getElementById("container");
 let c = 10;
 let result = actions
-<<<<<<< HEAD
-=======
-aaaaaaa
-<<<<<<< HEAD
-    .filter(act => {
-        console.log('aaaaaaaaaaa', act);
-        // let actYear = new Date(act.time).getFullYear();
-        // return actYear >= 2023;
-        let start2023 = new Date(2023, 0, 1).getTime();
-        return act.time >= start2023;
-    })
-    .sort((a, b) => {
-        if (a.time < b.time) return -1;
-        else return 1;
-    })
-    .forEach(act => {
-        let user = users.find(u => u.id == act.userId);
-        let lift = lifts.find(l => l.id == act.liftId);
-        // console.log('111111111111111111', user);
-        // act.userId
-        //     return `
-        // <p>Người dùng: ${user.name}</p>
-        // <p>Tên thang máy: ${lift ? lift.name : ''}</p>
-        // <p>Vào tầng: ${act.from}</p>
-        // <p>Ra tầng: ${act.to}</p>
-        // <p>Thời gian: năm ${new Date(act.time).getFullYear()} - tháng ${new Date(act.time).getMonth() + 1} - ngày ${new Date(act.time).getDate()} ${new Date(act.time).getHours()} : ${new Date(act.time).getMinutes()} - </p>
-        // `
+  .filter((act) => {
+    console.log("aaaaaaaaaaa", act);
+    // let actYear = new Date(act.time).getFullYear();
+    // return actYear >= 2023;
+    let start2023 = new Date(2023, 0, 1).getTime();
+    return act.time >= start2023;
+  })
+  .sort((a, b) => {
+    if (a.time < b.time) return -1;
+    else return 1;
+  })
+  .forEach((act) => {
+    let user = users.find((u) => u.id == act.userId);
+    let lift = lifts.find((l) => l.id == act.liftId);
+    // console.log('111111111111111111', user);
+    // act.userId
+    //     return `
+    // <p>Người dùng: ${user.name}</p>
+    // <p>Tên thang máy: ${lift ? lift.name : ''}</p>
+    // <p>Vào tầng: ${act.from}</p>
+    // <p>Ra tầng: ${act.to}</p>
+    // <p>Thời gian: năm ${new Date(act.time).getFullYear()} - tháng ${new Date(act.time).getMonth() + 1} - ngày ${new Date(act.time).getDate()} ${new Date(act.time).getHours()} : ${new Date(act.time).getMinutes()} - </p>
+    // `
 
-        // let p = document.createElement('li');
-        // p.innerText = `Người dùng: ${user.name}`;
-        // container.appendChild(p)
-        const tbl = document.getElementById("tbl");
-        const tblBody = document.createElement("tbody");
+    // let p = document.createElement('li');
+    // p.innerText = `Người dùng: ${user.name}`;
+    // container.appendChild(p)
+    const tbl = document.getElementById("tbl");
+    const tblBody = document.createElement("tbody");
 
-        // creating all cells
-        for (let i = 0; i < 1; i++) {
-            // creates a table row
-            const row = document.createElement("tr");
+    // creating all cells
+    for (let i = 0; i < 1; i++) {
+      // creates a table row
+      const row = document.createElement("tr");
 
-            // Create a <td> element and a text node, make the text
-            // node the contents of the <td>, and put the <td> at
-            // the end of the table row
-            const cell1 = document.createElement("td");
-            const cellText1 = document.createTextNode(`${user.name}`);
-            cell1.appendChild(cellText1);
-            row.appendChild(cell1);
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      const cell1 = document.createElement("td");
+      const cellText1 = document.createTextNode(`${user.name}`);
+      cell1.appendChild(cellText1);
+      row.appendChild(cell1);
 
-            // add the row to the end of the table body
-            tblBody.appendChild(row);
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
 
-            const cell2 = document.createElement("td");
-            const cellText2 = document.createTextNode(`${lift ? lift.name : ''}`);
-            cell2.appendChild(cellText2);
-            row.appendChild(cell2);
+      const cell2 = document.createElement("td");
+      const cellText2 = document.createTextNode(`${lift ? lift.name : ""}`);
+      cell2.appendChild(cellText2);
+      row.appendChild(cell2);
 
-            // add the row to the end of the table body
-            tblBody.appendChild(row);
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
 
-            const cell3 = document.createElement("td");
-            const cellText3 = document.createTextNode(`${act.from}`);
-            cell3.appendChild(cellText3);
-            row.appendChild(cell3);
+      const cell3 = document.createElement("td");
+      const cellText3 = document.createTextNode(`${act.from}`);
+      cell3.appendChild(cellText3);
+      row.appendChild(cell3);
 
-            // add the row to the end of the table body
-            tblBody.appendChild(row);
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
 
-            const cell4 = document.createElement("td");
-            const cellText4 = document.createTextNode(`${act.to}`);
-            cell4.appendChild(cellText4);
-            row.appendChild(cell4);
+      const cell4 = document.createElement("td");
+      const cellText4 = document.createTextNode(`${act.to}`);
+      cell4.appendChild(cellText4);
+      row.appendChild(cell4);
 
-            // add the row to the end of the table body
-            tblBody.appendChild(row);
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
 
-            const cell5 = document.createElement("td");
-            const cellText5 = document.createTextNode(`${new Date(act.time).getFullYear()}-${new Date(act.time).getMonth() + 1}-${new Date(act.time).getDate()} ${new Date(act.time).getHours()} : ${new Date(act.time).getMinutes()}`);
-            cell5.appendChild(cellText5);
-            row.appendChild(cell5);
+      const cell5 = document.createElement("td");
+      const cellText5 = document.createTextNode(
+        `${new Date(act.time).getFullYear()}-${
+          new Date(act.time).getMonth() + 1
+        }-${new Date(act.time).getDate()} ${new Date(
+          act.time
+        ).getHours()} : ${new Date(act.time).getMinutes()}`
+      );
+      cell5.appendChild(cellText5);
+      row.appendChild(cell5);
 
-            // add the row to the end of the table body
-            tblBody.appendChild(row);
-        }
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
 
-        // put the <tbody> in the <table>
-        tbl.appendChild(tblBody);
-        // appends <table> into <body>
-        document.body.appendChild(tbl);
-        // sets the border attribute of tbl to '2'
-        tbl.setAttribute("border", "2");
-    })
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    document.body.appendChild(tbl);
+    // sets the border attribute of tbl to '2'
+    tbl.setAttribute("border", "2");
+  })
 
-// console.log('aaaaaaaaaaaaaaaaaaaaaaa', result);
-// BTVN tạo bảng hiển thị thông tin
-=======
->>>>>>> 56e29f330a0bed609cb4e80ccb5d673ea1f228ec
+  // console.log('aaaaaaaaaaaaaaaaaaaaaaa', result);
+  // BTVN tạo bảng hiển thị thông tin
   .filter((act) => {
     console.log("aaaaaaaaaaa", act);
     // let actYear = new Date(act.time).getFullYear();
@@ -285,8 +254,3 @@ aaaaaaa
     p.innerText = `Người dùng: ${user.name}`;
     container.appendChild(p);
   });
-<<<<<<< HEAD
-let a = 3;
-=======
->>>>>>> 846e642f325e131847536a75d0fa651362dfa455
->>>>>>> 56e29f330a0bed609cb4e80ccb5d673ea1f228ec
