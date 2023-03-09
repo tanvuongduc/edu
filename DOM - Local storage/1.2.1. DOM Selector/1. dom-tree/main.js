a05a384963b243aff6724effdf5a202c80d67ea7
hoanghieu95 <vuhoanghieu95@gmail.com>
2023-03-09 19:14:13 +0700

    09/03
    09/03

--- DOM - Local storage/1.2.1. DOM Selector/1. dom-tree/main.js
+++ DOM - Local storage/1.2.1. DOM Selector/1. dom-tree/main.js
@@ -15,15 +15,32 @@
 // let a = document.getElementById('id0')
 // let b = document.getElementsByClassName('title')
 // console.log(a, b);
-let a = 2;
-let b = 3;
-let cat = "love arsenal";
-let dog = "love cat";
-let animal = "love animal";
-let car = "love car";
-let f = 4;
-let d = 4;
-let e = 5;
+let lifts = [
+  {
+    id: 1,
+    name: "Thang máy 1",
+    status: true,
+    currentFloor: 1,
+  },
+  {
+    id: 2,
+    name: "Thang máy 2",
+    status: false,
+    currentFloor: 15,
+  },
+  {
+    id: 3,
+    name: "Thang máy 3",
+    status: true,
+    currentFloor: 10,
+  },
+  {
+    id: 4,
+    name: "Thang máy 4",
+    status: true,
+    currentFloor: 19,
+  },
+];
 let users = [
   {
     id: 1,
@@ -46,33 +63,6 @@ let users = [
     gender: true,
   },
 ];
-let lifts = [
-  {
-    id: "1",
-    name: "Thang máy 1",
-    status: true,
-    currentFloor: 1,
-  },
-  {
-    id: "2",
-    name: "Thang máy 2",
-    status: false,
-    currentFloor: 15,
-  },
-  {
-    id: "3",
-    name: "Thang máy 3",
-    status: true,
-    currentFloor: 10,
-  },
-  {
-    id: "4",
-    name: "Thang máy 4",
-    status: true,
-    currentFloor: 19,
-  },
-];
-
 let actions = [
   {
     id: 1,
@@ -80,7 +70,7 @@ let actions = [
     liftId: 1,
     from: 1,
     to: 10,
-    time: new Date(2023, 0, 2).getTime(),
+    time: new Date(2023, 1, 2).getTime(),
   },
   {
     id: 2,
@@ -132,101 +122,85 @@ let actions = [
   },
 ];
 
-let container = document.getElementById("container");
-let c = 10;
-let result = actions
-  .filter((act) => {
-    console.log("aaaaaaaaaaa", act);
-    // let actYear = new Date(act.time).getFullYear();
-    // return actYear >= 2023;
-    let start2023 = new Date(2023, 0, 1).getTime();
-    return act.time >= start2023;
-  })
-  .sort((a, b) => {
-    if (a.time < b.time) return -1;
-    else return 1;
-  })
-  .forEach((act) => {
-    let user = users.find((u) => u.id == act.userId);
-    let lift = lifts.find((l) => l.id == act.liftId);
-    // console.log('111111111111111111', user);
-    // act.userId
-    //     return `
-    // <p>Người dùng: ${user.name}</p>
-    // <p>Tên thang máy: ${lift ? lift.name : ''}</p>
-    // <p>Vào tầng: ${act.from}</p>
-    // <p>Ra tầng: ${act.to}</p>
-    // <p>Thời gian: năm ${new Date(act.time).getFullYear()} - tháng ${new Date(act.time).getMonth() + 1} - ngày ${new Date(act.time).getDate()} ${new Date(act.time).getHours()} : ${new Date(act.time).getMinutes()} - </p>
-    // `
+function render() {
+  let list = `<tr>
+      <th>ID</th>
+      <th>Vào tầng</th>
+      <th>Ra tầng</th>
+      <th>Thời gian</th>
+      <th>Tùy chọn</th>
+  </tr>`;
 
-    // let p = document.createElement('li');
-    // p.innerText = `Người dùng: ${user.name}`;
-    // container.appendChild(p)
-    const tbl = document.getElementById("tbl");
-    const tblBody = document.createElement("tbody");
-
-    // BTVN tạo bảng hiển thị thông tin
-    // creating all cells
-    for (let i = 0; i < 1; i++) {
-      // creates a table row
-      const row = document.createElement("tr");
-
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      const cell1 = document.createElement("td");
-      const cellText1 = document.createTextNode(`${user.name}`);
-      cell1.appendChild(cellText1);
-      row.appendChild(cell1);
-
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-
-      const cell2 = document.createElement("td");
-      const cellText2 = document.createTextNode(`${lift ? lift.name : ""}`);
-      cell2.appendChild(cellText2);
-      row.appendChild(cell2);
-
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-
-      const cell3 = document.createElement("td");
-      const cellText3 = document.createTextNode(`${act.from}`);
-      cell3.appendChild(cellText3);
-      row.appendChild(cell3);
-
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-
-      const cell4 = document.createElement("td");
-      const cellText4 = document.createTextNode(`${act.to}`);
-      cell4.appendChild(cellText4);
-      row.appendChild(cell4);
-
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-
-      const cell5 = document.createElement("td");
-      const cellText5 = document.createTextNode(
-        `${new Date(act.time).getFullYear()}-${
-          new Date(act.time).getMonth() + 1
-        }-${new Date(act.time).getDate()} ${new Date(
-          act.time
-        ).getHours()} : ${new Date(act.time).getMinutes()}`
-      );
-      cell5.appendChild(cellText5);
-      row.appendChild(cell5);
-
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-    }
-
-    // put the <tbody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    document.body.appendChild(tbl);
-    // sets the border attribute of tbl to '2'
-    tbl.setAttribute("border", "2");
+  actions.map((a, i) => {
+    list += `<tr>
+      <td>${a.id}</td>
+      <td>${a.from}</td>
+      <td>${a.to}</td>
+      <td>
+        ${new Date(a.time).getFullYear()} -
+        ${new Date(a.time).getMonth()} -
+        ${new Date(a.time).getDate()}
+    </td>
+      <td>
+        <button id="btn-detail" onclick="addDetail(${i})">Detail</button>
+        <button id="btn-delete" onclick="deleteDetail(${
+          a.id
+        })">Delete</button>
+      </td>
+    </tr>`;
   });
+  document.getElementById("table").innerHTML = list;
+}
+render();
 
-// console.log('aaaaaaaaaaaaaaaaaaaaaaa', result);
+const listDetail = [];
+
+function addDetail(i) {
+  listDetail.unshift(actions[i]);
+  renderDetail();
+  // console.log(listDetail);
+}
+
+function deleteDetail(i) {
+  console.log(i);
+  console.log(listDetail.length);
+  if (listDetail.length > 0) {
+    listDetail.map((val, index) => {
+      if (val.id == i) {
+        console.log(index);
+        listDetail.splice(index, 1);
+      }
+    });
+    renderDetail();
+  } else {
+    alert("Không thể xóa");
+  }
+  console.log(listDetail);
+}
+
+function renderDetail() {
+  let list = `<th>Thông tin chi tiết</th>`;
+  listDetail.map((val, i) => {
+    let user = users.find((u) => {
+      if (u.id == val.userId) return true; //${user.name}
+    });
+    let lift = lifts.find((lif) => {
+      if (lif.id == val.liftId) return true; //${lift.name}
+    });
+    list += `<tr>
+      <td>
+        <p>ID: ${val.id}</p>
+        <p>Người dùng: ${user.name}</p>
+        <p>Tên thang máy: ${lift ? lift.name : ""}</p>
+        <p>Vào tầng: ${val.from}</p>
+        <p>Ra tầng: ${val.to}</p>
+        <p>Thời gian: năm ${new Date(val.time).getFullYear()} -
+          tháng ${new Date(val.time).getMonth()} -
+          ngày ${new Date(val.time).getDate()}
+        </p>
+        <p>Ghi chú: <input type="text" id="note" /></p>
+      </td>
+      </tr>`;
+  });
+  document.getElementById("tableDetail").innerHTML = list;
+}
