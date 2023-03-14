eb964f9e992c4cc3ba9c4861d3246ccfddc3900e
hoanghieu95 <vuhoanghieu95@gmail.com>
2023-03-14 16:57:29 +0700

    14/03
    14/03

--- DOM - Local storage/1.2.1. DOM Selector/1. dom-tree/main.js
+++ DOM - Local storage/1.2.1. DOM Selector/1. dom-tree/main.js
@@ -15,32 +15,12 @@
 // let a = document.getElementById('id0')
 // let b = document.getElementsByClassName('title')
 // console.log(a, b);
-let lifts = [
-  {
-    id: 1,
-    name: "Thang máy 1",
-    status: true,
-    currentFloor: 1,
-  },
-  {
-    id: 2,
-    name: "Thang máy 2",
-    status: false,
-    currentFloor: 15,
-  },
-  {
-    id: 3,
-    name: "Thang máy 3",
-    status: true,
-    currentFloor: 10,
-  },
-  {
-    id: 4,
-    name: "Thang máy 4",
-    status: true,
-    currentFloor: 19,
-  },
-];
+let a = 2;
+let b = 3;
+
+let cat = "love dog";
+let dog = "love cat";
+
 let users = [
   {
     id: 1,
@@ -70,7 +50,7 @@ let actions = [
     liftId: 1,
     from: 1,
     to: 10,
-    time: new Date(2023, 1, 2).getTime(),
+    time: new Date(2023, 0, 2).getTime(),
   },
   {
     id: 2,
@@ -79,6 +59,7 @@ let actions = [
     from: 2,
     to: 10,
     time: new Date(2022, 3, 5).getTime(),
+    note: "",
   },
   {
     id: 3,
@@ -87,6 +68,7 @@ let actions = [
     from: 3,
     to: 10,
     time: new Date(2021, 9, 10).getTime(),
+    note: "",
   },
   {
     id: 4,
@@ -95,6 +77,7 @@ let actions = [
     from: 4,
     to: 10,
     time: new Date(2023, 1, 20).getTime(),
+    note: "",
   },
   {
     id: 5,
@@ -103,6 +86,7 @@ let actions = [
     from: 5,
     to: 10,
     time: new Date(2023, 1, 20).getTime(),
+    note: "",
   },
   {
     id: 6,
@@ -111,6 +95,7 @@ let actions = [
     from: 6,
     to: 10,
     time: new Date(2019, 1, 22).getTime(),
+    note: "",
   },
   {
     id: 7,
@@ -119,6 +104,7 @@ let actions = [
     from: 7,
     to: 10,
     time: new Date(2023, 1, 28).getTime(),
+    note: "",
   },
 ];
 
@@ -131,76 +117,513 @@ function render() {
       <th>Tùy chọn</th>
   </tr>`;
 
-  actions.map((a, i) => {
-    list += `<tr>
-      <td>${a.id}</td>
-      <td>${a.from}</td>
-      <td>${a.to}</td>
-      <td>
-        ${new Date(a.time).getFullYear()} -
-        ${new Date(a.time).getMonth()} -
-        ${new Date(a.time).getDate()}
-    </td>
-      <td>
-        <button id="btn-detail" onclick="addDetail(${i})">Detail</button>
-        <button id="btn-delete" onclick="deleteDetail(${
-          a.id
-        })">Delete</button>
-      </td>
-    </tr>`;
-  });
-  document.getElementById("table").innerHTML = list;
-}
-render();
+    const cell3 = document.createElement("td");
+    cell3.innerText = `${act.from}`;
+    row.appendChild(cell3);
 
-const listDetail = [];
+    // add the row to the end of the table body
+    // tblBody.appendChild(row);
 
-function addDetail(i) {
-  listDetail.unshift(actions[i]);
-  renderDetail();
-  // console.log(listDetail);
-}
+    const cell4 = document.createElement("td");
+    cell4.innerText = `${act.to}`;
+    row.appendChild(cell4);
 
-function deleteDetail(i) {
-  console.log(i);
-  console.log(listDetail.length);
-  if (listDetail.length > 0) {
-    listDetail.map((val, index) => {
-      if (val.id == i) {
-        console.log(index);
-        listDetail.splice(index, 1);
-      }
-    });
-    renderDetail();
-  } else {
-    alert("Không thể xóa");
-  }
-  console.log(listDetail);
-}
+    const cell5 = document.createElement("td");
+    cell5.innerText = `${new Date(act.time).getFullYear()}-${
+      new Date(act.time).getMonth() + 1
+    }-${new Date(act.time).getDate()} ${new Date(
+      act.time
+    ).getHours()} : ${new Date(act.time).getMinutes()}`;
+    row.appendChild(cell5);
 
-function renderDetail() {
-  let list = `<th>Thông tin chi tiết</th>`;
-  listDetail.map((val, i) => {
-    let user = users.find((u) => {
-      if (u.id == val.userId) return true; //${user.name}
+    const cell6 = document.createElement("td");
+    const btn1 = document.createElement("button");
+    const btn2 = document.createElement("button");
+    btn1.innerText = `Chi tiết`;
+    btn2.innerText = `Xóa`;
+
+    btn1.addEventListener("click", () => {
+      document.getElementById("text").innerHTML = `Tên người dùng: ${
+        user.name
+      }; Tên thang máy: ${lift ? lift.name : ""}; Ghi chú: ${
+        act.note ? act.note : "Chưa có ghi chú"
+      }`;
+      document.getElementById("note").innerHTML = `Sửa ghi chú: `;
+      const inp = document.createElement("input");
+      const btn3 = document.createElement("button");
+      btn3.innerText = ` Sửa `;
+      document.getElementById("note").appendChild(inp);
+      document.getElementById("note").appendChild(btn3);
     });
     let lift = lifts.find((lif) => {
       if (lif.id == val.liftId) return true; //${lift.name}
     });
-    list += `<tr>
-      <td>
-        <p>ID: ${val.id}</p>
-        <p>Người dùng: ${user.name}</p>
-        <p>Tên thang máy: ${lift ? lift.name : ""}</p>
-        <p>Vào tầng: ${val.from}</p>
-        <p>Ra tầng: ${val.to}</p>
-        <p>Thời gian: năm ${new Date(val.time).getFullYear()} -
-          tháng ${new Date(val.time).getMonth()} -
-          ngày ${new Date(val.time).getDate()}
-        </p>
-        <p>Ghi chú: <input type="text" id="note" /></p>
-      </td>
-      </tr>`;
+
+    cell6.appendChild(btn1);
+    cell6.appendChild(btn2);
+    row.appendChild(cell6);
+
+    // put the <tbody> in the <table>
+    tblBody.appendChild(row);
+    tbl.setAttribute("border", "2");
+    tbl.appendChild(tblBody);
   });
-  document.getElementById("tableDetail").innerHTML = list;
-}
+  let lifts = [
+    {
+      id: 1,
+      name: "Thang máy 1",
+      status: true,
+      currentFloor: 1,
+    },
+    {
+      id: 2,
+      name: "Thang máy 2",
+      status: false,
+      currentFloor: 15,
+    },
+    {
+      id: 3,
+      name: "Thang máy 3",
+      status: true,
+      currentFloor: 10,
+    },
+    {
+      id: 4,
+      name: "Thang máy 4",
+      status: true,
+      currentFloor: 19,
+    },
+  ];
+  let users = [
+    {
+      id: 1,
+      name: "tan",
+      gender: true,
+    },
+    {
+      id: 2,
+      name: "phuong",
+      gender: false,
+    },
+    {
+      id: 3,
+      name: "thanh",
+      gender: true,
+    },
+    {
+      id: 4,
+      name: "minh",
+      gender: true,
+    },
+  ];
+  let actions = [
+    {
+      id: 1,
+      userId: 1,
+      liftId: 1,
+      from: 1,
+      to: 10,
+      time: new Date(2023, 1, 2).getTime(),
+      note: "",
+    },
+    {
+      id: 2,
+      userId: 2,
+      liftId: 2,
+      from: 2,
+      to: 10,
+      time: new Date(2022, 3, 5).getTime(),
+      note: "",
+    },
+    {
+      id: 3,
+      userId: 3,
+      liftId: 3,
+      from: 3,
+      to: 10,
+      time: new Date(2021, 9, 10).getTime(),
+      note: "",
+    },
+    {
+      id: 4,
+      userId: 4,
+      liftId: 4,
+      from: 4,
+      to: 10,
+      time: new Date(2023, 1, 20).getTime(),
+      note: "",
+    },
+    {
+      id: 5,
+      userId: 1,
+      liftId: 2,
+      from: 5,
+      to: 10,
+      time: new Date(2023, 1, 20).getTime(),
+      note: "",
+    },
+    {
+      id: 6,
+      userId: 2,
+      liftId: 3,
+      from: 6,
+      to: 10,
+      time: new Date(2019, 1, 22).getTime(),
+      note: "",
+    },
+    {
+      id: 7,
+      userId: 3,
+      liftId: 4,
+      from: 7,
+      to: 10,
+      time: new Date(2023, 1, 28).getTime(),
+      note: "",
+    },
+  ];
+  
+  const listDetail = [];
+  
+  function render() {
+    let list = `<tr>
+              <th>ID</th>
+              <th>Vào tầng</th>
+              <th>Ra tầng</th>
+              <th>Thời gian</th>
+              <th>Tùy chọn</th>
+          </tr>`;
+  
+    actions.map((a, i) => {
+      list += `<tr>
+              <td>${a.id}</td>
+              <td>${a.from}</td>
+              <td>${a.to}</td>
+              <td>
+                ${new Date(a.time).getFullYear()} -
+                ${new Date(a.time).getMonth() + 1} -
+                ${new Date(a.time).getDate()}
+            </td>
+              <td>
+                <button id="btn-detail" onclick="addDetail(${i})">Detail</button>
+                <button id="btn-delete" onclick="deleteRow(${i})">Delete</button>
+              </td>
+            </tr>`;
+    });
+    document.getElementById("table").innerHTML = list;
+  }
+  render();
+  
+  function renderDetail() {
+    let list = ``;
+    listDetail.map((val, i) => {
+      let user = users.find((u) => {
+        if (u.id == val.userId) return true; //${user.name}
+      });
+      let lift = lifts.find((lif) => {
+        if (lif.id == val.liftId) return true; //${lift.name}
+      });
+      list = `<tr>
+              <td>
+                <p>Người dùng: ${user.name}</p>
+                <p>Tên thang máy: ${lift ? lift.name : ""}</p>
+                <p>Vào tầng: ${val.from}</p>
+                <p>Ra tầng: ${val.to}</p>
+                <p>Thời gian: năm ${new Date(val.time).getFullYear()} -
+                  tháng ${new Date(val.time).getMonth() + 1} -
+                  ngày ${new Date(val.time).getDate()}
+                </p>
+                <p>Ghi chú:  ${val.note ? val.note : ""}</p>
+                <p>
+                  Ghi chú:
+                  <input type="text" id="Note" />
+                  <button id="btn-add-note" onclick="addNote(${i})">Note</button>
+                </p>
+                <p><button id="btn-delete-list" onclick="clearList(${i})">Clear</button></p>
+              </td>
+              </tr>`;
+    });
+    document.getElementById("tableDetail").innerHTML = list;
+  }
+  
+  function renderAddInfo(val) {
+    var list = ``;
+    if (val == 1) {
+      list += `<tr>
+          <td>
+            <p>id: <label id="iddemo"></label> <button onclick="getID(${val})">Get ID</button></p>
+            <p>name: <input type="text" id="liftName" /></p>
+            <p>status: 
+            <select
+              name="selectStatus"
+              id="selectStatus"
+              onchange="selectedStatus(this)"
+            >
+              <option value="" >-- Chọn trạng thái --</option>
+              <option value="true" >true</option>
+              <option value="false">false</option>
+            </select>
+            </p>
+            <p>currentFloor: <input type="text" id="liftCurrentFloor"/></p>
+            <p>
+              <button id="btn-add-lift" onclick="addLift()">Add Lift</button> || 
+              <button id="btn-delete-lift" onclick="clearLift()">Clear</button>
+            </p>
+          </td>
+          </tr>`;
+    } else if (val == 2) {
+      list += `<tr>
+          <td>
+            <p>id: <label id="iddemo"></label> <button onclick="getID(${val})">Get ID</button></p>
+            <p>name: <input type="text" id="userName" /></p>
+            <p>gender: 
+            <select
+              name="selectGender"
+              id="selectGender"
+              onchange="selectedGender(this)"
+            >
+              <option value="" >-- Chọn giới tính --</option>
+              <option value="true" >Nam</option>
+              <option value="false">Nu</option>
+            </select>
+            </p>
+            <p>
+              <button id="btn-add-lift" onclick="addUser()">Add User</button> ||
+              <button id="btn-delete-lift" onclick="clearUser()">Clear</button>
+            </p>
+          </td>
+          </tr>`;
+    } else if (val == 3) {
+      list += `<tr>
+          <td>
+            <p>id: <label id="iddemo"></label> <button onclick="getID(${val})">Get ID</button></p>
+            <p>userId: 
+            <select
+              name="selectUserId"
+              id="selectUserId"
+              onchange="selectedUserId(this)"
+            >
+              <option value="" >-- Chọn user --</option>
+              ${users.map((u, i) => {
+                return `<option value="${u.id}" >${u.id} - ${u.name}</option>`;
+              })}
+            </select>
+            </p>
+            <p>liftId: 
+            <select
+              name="selectLiftId"
+              id="selectLiftId"
+              onchange="selectedLiftId(this)"
+            >
+            <option value="" >-- Chọn lift --</option>
+            ${lifts.map((l, i) => {
+              return `<option value="${l.id}" >${l.id} - ${l.name}</option>`;
+            })}
+            </select>
+            </p>
+            <p>from: <input type="text" id="from" /></p>
+            <p>to: <input type="text" id="to" /></p>
+            <p>time: <label id="time"></label> <button onclick="getTime()">Get Time</button></p>
+            <p>note: <input type="text" id="note" /></p>
+            <p>
+              <button id="btn-add-lift" onclick="addAction()">Add Action</button> || 
+              <button id="btn-delete-lift" onclick="clearAction()">Clear</button>
+            </p>
+          </td>
+          </tr>`;
+    }
+    document.getElementById("addInfo").innerHTML = list;
+  }
+  
+  function addDetail(i) {
+    if ((listDetail.length = 0)) {
+      listDetail.unshift(actions[i]);
+      renderDetail();
+    } else {
+      listDetail.pop();
+      listDetail.unshift(actions[i]);
+      renderDetail();
+    }
+  }
+  
+  function addNote(i) {
+    let Note = document.getElementById("Note").value;
+    if (Note) {
+      listDetail[i].note = Note;
+      console.log((listDetail[i].note = Note));
+    }
+    document.getElementById("Note").value = "";
+    renderDetail();
+  }
+  
+  function clearList(i) {
+    listDetail.pop();
+    renderDetail();
+  }
+  
+  function deleteRow(i) {
+    actions.splice(i, 1);
+    render();
+    // if (listDetail.length > 0) {
+    //   listDetail.map((val, index) => {
+    //     if (val.id == i) {
+    //       listDetail.splice(index, 1);
+    //     }
+    //   });
+    //   renderDetail();
+    // }
+  }
+  
+  function validateSelectBox(obj) {
+    var options = obj.children;
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        renderAddInfo(options[i].value);
+      }
+    }
+  }
+  
+  function getID(val) {
+    if (val == 1) {
+      lifts.map((value, i) => {
+        if (Math.floor(Math.random() * 100) !== value.id) {
+          // console.log(Math.random() * 100 !== value.id);
+          return true;
+        }
+      });
+    } else if (val == 2) {
+      users.map((value, i) => {
+        if (Math.floor(Math.random() * 100) !== value.id) {
+          // console.log(Math.random() * 100 !== value.id);
+          return true;
+        }
+      });
+    } else if (val == 3) {
+      actions.map((value, i) => {
+        if (Math.floor(Math.random() * 100) !== value.id) {
+          // console.log(Math.random() * 100 !== value.id);
+          return true;
+        }
+      });
+    }
+    document.getElementById("iddemo").innerHTML = Math.floor(Math.random() * 100);
+  }
+  
+  function getTime() {
+    var time =
+      new Date().getFullYear() +
+      "-" +
+      (new Date().getMonth() + 1) +
+      "-" +
+      new Date().getDate();
+    document.getElementById("time").innerHTML = time;
+  }
+  
+  function selectedStatus(obj) {
+    var options = obj.children;
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        console.log(options[i].value);
+        return options[i].value;
+      }
+    }
+  }
+  
+  function selectedGender(obj) {
+    var options = obj.children;
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        console.log(options[i].value);
+        return options[i].value;
+      }
+    }
+  }
+  
+  function selectedUserId(obj) {
+    var options = obj.children;
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        // console.log(options[i].value);
+        return options[i].value;
+      }
+    }
+  }
+  
+  function selectedLiftId(obj) {
+    var options = obj.children;
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        // console.log(options[i].value);
+        return options[i].value;
+      }
+    }
+  }
+  
+  function addLift() {
+    var newLift = {
+      id: document.getElementById("iddemo").innerHTML,
+      name: document.getElementById("liftName").value,
+      status: document.getElementById("selectStatus").value,
+      currentFloor: document.getElementById("liftCurrentFloor").value,
+    };
+    console.log(lifts)
+    lifts.push(newLift);
+    console.log(lifts)
+    clearLift();
+  }
+  
+  function addUser() {
+    var newUser = {
+      id: document.getElementById("iddemo").innerHTML,
+      name: document.getElementById("userName").value,
+      gender: document.getElementById("selectGender").value,
+    };
+    console.log(users)
+    users.push(newUser);
+    console.log(users)
+    clearUser();
+  }
+  
+  function addAction() {
+    var newAction = {
+      id: document.getElementById("iddemo").innerHTML,
+      userId: document.getElementById("selectUserId").value,
+      liftId: document.getElementById("selectLiftId").value,
+      from: document.getElementById("from").value,
+      to: document.getElementById("to").value,
+      time: document.getElementById("time").innerHTML,
+      note: document.getElementById("note").value,
+    };
+    actions.push(newAction);
+    render();
+    clearAction();
+  }
+  
+  function clearLift() {
+    var newLift = {
+      id: (document.getElementById("iddemo").innerHTML = ""),
+      name: (document.getElementById("liftName").value = ""),
+      status: (document.getElementById("selectStatus").value = ""),
+      currentFloor: (document.getElementById("liftCurrentFloor").value = ""),
+    };
+  }
+  
+  function clearUser() {
+    var newUser = {
+      id: (document.getElementById("iddemo").innerHTML = ""),
+      name: (document.getElementById("userName").value = ""),
+      gender: (document.getElementById("selectGender").value = ""),
+    };
+  }
+  
+  function clearAction() {
+    var newAction = {
+      id: (document.getElementById("iddemo").innerHTML = ""),
+      userId: (document.getElementById("selectUserId").value = ""),
+      liftId: (document.getElementById("selectLiftId").value = ""),
+      from: (document.getElementById("from").value = ""),
+      to: (document.getElementById("to").value = ""),
+      time: (document.getElementById("time").innerHTML = ""),
+      note: (document.getElementById("note").value = ""),
+    };
+  }
+  
