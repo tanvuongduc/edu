3ac84fbf267aad33718c12fda8de208090c06315
hieuphan <you@example.com>
2023-04-18 23:19:35 +0700

    .

--- DOM - Local storage/1.3. HTTP API/fetch/main.js
+++ DOM - Local storage/1.3. HTTP API/fetch/main.js
@@ -1,138 +1,90 @@
-// what is callback
-// setTimeout(() => console.log('hello'), 1000);
+fetch('https://jsonplaceholder.typicode.com/posts', options1).then(res => {
+    // console.log('bbbbbbbbbbbbbbbbbbb', res);
+    res.json().then(_res => {
+        console.log('aaa', _res);
 
-
-// using fetch api
-// function readAllTodo(callback) {
-//     fetch('https://jsonplaceholder.typicode.com/users/1/todos').then(r => r.json()).then(d => callback(d));
-// }
-
-// function readAllTodo(callback) {
-//     fetch('https://jsonplaceholder.typicode.com/users/1/todos').then(r => r.json()).then(d => callback(d));
-// }
-
-// // using fetch api
-// function createTodo(callback) {
-//     fetch('https://jsonplaceholder.typicode.com/todos', {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             title: 'Task 1',
-//             userId: 1000,
-//         }),
-//         headers: {
-//             'Content-type': 'application/json; charset=UTF-8',
-//             'tan': 'dep trai'
-//         },
-//     }).then(d => callback(d))
-//         .catch(err => {
-
-//         });
-// }
-// createTodo((d) => console.log(d));
-
-// // GET lấy dữ liệu
-// // POST Lưu dữ liệu
-// // PUT Sửa dữ liệu
-// // PATCH Sửa dữ liệu
-// // DELETE Xóa dữ liệu
-
-function doCallAPI() {
-    // console.log('aaaaaaaaaaaaaaaaaaaa');
-}
-
-function callAPI() {
-    doCallAPI()
-}
-
-function createTodo(callback) {
-    callAPI();
-}
-
-createTodo();
-
-let options1 = {
-    method: 'POST',
-    body: JSON.stringify({
-        title: 'Task 1',
-        userId: 1000,
-    }),
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-        'tan': 'dep trai'
-    },
-};
-let options2 = {
-    method: 'GET',
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-        'tan': 'dep trai'
-    },
-};
-// console.log('1111111111111111111');
-// console.log('22222222222222222');
-// console.log('33333333333333333333');
-
-// fetch('https://jsonplaceholder.typicode.com/todos', options1).then(res => {
-//     // console.log('bbbbbbbbbbbbbbbbbbb', res);
-//     res.json().then(_res => {
-//         console.log('ddddddddddddddddd', _res);
-
-//     });
-// }).catch(err => {
-//     console.log('ccccccccccccccccccccc', err);
-// })
-// fetch('https://jsonplaceholder.typicode.com/todos', options2).then(res => {
-//     // console.log('bbbbbbbbbbbbbbbbbbb', res);
-//     res.json().then(_res => {
-//         console.log('eeeeeeeeeeeeeeee', _res);
-
-//     });
-// }).catch(err => {
-//     console.log('ccccccccccccccccccccc', err);
-// })
-// Promise.all([
-//     fetch('https://jsonplaceholder.typicode.com/todos', options1),
-//     fetch('https://jsonplaceholder.typicode.com/todos', options2)
-// ]).then(res => {
-//     console.log('aaaaaaaaaaaaaaaaaa', res);
-//     Promise.all([res[0].json(), res[1].json()]).then(_res => {
-
-//     console.log('bbbbbbbbbbbbbbbb', _res);
-//     })
-// }).catch(err => {
-
-// })
-
-// function callback1() {
-//     console.log('aaaaaaaaaaaaaaaaa');
-// }
-
-// function callback2() {
-//     console.log('ccccccccccccccccccc');
-// }
-
-// function test(cb) {
-//     console.log('bbbbbbbbbbbbbbbbbbb', cb);
-//     cb();
-// }
-
-// test(callback2)
-let promise = new Promise((resolve, reject) => {
-    if (1 == 1) {
-        setTimeout(() => {
-            resolve('hehehe');
-        }, 5000);
-    } else {
-        reject('huhuhu');
-    }
-});
-promise.then(res => {
-    console.log('aaaaaaaaaaaaaaa', res);
+    });
 }).catch(err => {
-    console.log('bbbbbbbbbbbbbbbbbbbbbb', err);
-
-}).finally(() => {
-    
+    console.log('zzz', err);
 })
-// console.log('aaaaaaaaaaaaaaaaaa');
-// console.log('444444444444444444444444');
+fetch('https://jsonplaceholder.typicode.com/comments', options2).then(res => {
+    // console.log('bbbbbbbbbbbbbbbbbbb', res);
+    res.json().then(_res => {
+        console.log('bbb', _res);
+
+    });
+}).catch(err => {
+    console.log('zzz', err);
+})
+fetch('https://jsonplaceholder.typicode.com/albums', options3).then(res => {
+    // console.log('bbbbbbbbbbbbbbbbbbb', res);
+    res.json().then(_res => {
+        console.log('ccc', _res);
+
+    });
+}).catch(err => {
+    console.log('zzz', err);
+})
+fetch('https://jsonplaceholder.typicode.com/photos', options4).then(res => {
+    // console.log('bbbbbbbbbbbbbbbbbbb', res);
+    res.json().then(_res => {
+        console.log('ddd', _res);
+
+    });
+}).catch(err => {
+    console.log('zzz', err);
+})
+fetch('https://jsonplaceholder.typicode.com/todos', options5).then(res => {
+    // console.log('bbbbbbbbbbbbbbbbbbb', res);
+    res.json().then(_res => {
+        console.log('zzz', _res);
+
+    });
+}).catch(err => {
+    console.log('fff', err);
+})
+fetch('https://jsonplaceholder.typicode.com/users', options6).then(res => {
+    // console.log('bbbbbbbbbbbbbbbbbbb', res);
+    res.json().then(_res => {
+        console.log('eee', _res);
+
+    });
+}).catch(err => {
+    console.log('zzz', err);
+})
+Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts', options1),
+    fetch('https://jsonplaceholder.typicode.com/comments', options2),
+    fetch('https://jsonplaceholder.typicode.com/albums', options3),
+    fetch('https://jsonplaceholder.typicode.com/photos', options4),
+    fetch('https://jsonplaceholder.typicode.com/todos', options5),
+    fetch('https://jsonplaceholder.typicode.com/users', options6)
+]).then(res => {
+    console.log('aaaaaaaaaaaaaaaaaa', res);
+    Promise.all([res[0].json(), res[1].json(),res[2].json(),res[3].json(),res[4].json(),res[5].json()]).then(_res => {
+
+    console.log('xxx', _res);
+    })
+}).catch(err => {
+
+})
+//F8
+// var postApi = 'https://jsonplaceholder.typicode.com/posts';
+// fetch(postApi)
+// .then(function(reponse){
+//     return reponse.json();
+// })
+// .then(function(posts){
+//     var htmls = posts.map(function(post){
+//         return `
+//        <li>
+//         <h2>${post.title}</h2>
+//         <p>${post.body}</p>
+//         </li>`;
+//     });
+//     var html = htmls.join('');
+//     document.getElementById('post-block').innerHTML = html;
+// })
+// .catch(function(err){
+//     console.log('co loi');
+// })
--- NodeJS/HTTP_Server/basic.js
+++ NodeJS/HTTP_Server/basic.js
@@ -1,33 +1,71 @@
 const express = require('express');
 const app = express();
-
+let users = [
+    {
+        id: 1,
+        name: "tan",
+        gender: true,
+        roleId: 1,
+    },
+    {
+        id: 2,
+        name: "phuong",
+        gender: false,
+        roleId: 2,
+    },
+    {
+        id: 3,
+        name: "thanh",
+        gender: true,
+        roleId: 4,
+    },
+    {
+        id: 4,
+        name: "minh",
+        gender: true,
+        roleId: 5,
+    },
+];
+app.use(express.json())
 app.get('/hello', (req, res) => {
     res.end('hello');
 });
-
+app.get('/users', (req, res) =>{
+    res.statusCode(200).send(users);
+})
+app.post('/users', (req, res) =>{
+    console.log(req.body)
+    let id = Math.ceil(Math.random() * 1000);
+    users.push({
+        id,
+        name: req.body.name,
+        gender: req.body.gender,
+        roleId: req.body.roleId
+    })
+})
 app.get('/hello/:name', (req, res) => {
     console.log('aaaaaaaaaaaaaaaaaa', req.method, req.url);
     res.end(`hello ${req.params.name}`);
 });
 
-app.use(express.json());
-const users = [
-    { username: "admin", password: "123456" }
-];
-const all_tokens = {};
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    for (const u of users) {
-        if (u.username === username && u.password === password) {
-            const token = Math.random().toFixed(6);
-            all_tokens[token] = u;
-            res.json({ ok: true, token: token });
-            return;
-        }
-    }
-    res.json({ ok: false });
-});
+// app.use(express.json());
+// const users = [
+//     { username: "admin", password: "123456" }
+// ];
+// const all_tokens = {};
+// app.post('/login', (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     for (const u of users) {
+//         if (u.username === username && u.password === password) {
+//             const token = Math.random().toFixed(6);
+//             all_tokens[token] = u;
+//             res.json({ ok: true, token: token });
+//             return;
+//         }
+//     }
+//     res.json({ ok: false });
+// });
 
 app.get('/info/:token', (req, res) => {
     const user = all_tokens[req.params.token];
